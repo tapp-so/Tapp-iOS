7ee7d1ea392877771db56366c38934b5e984f18e
alex_stergiou <alex.stergiou@hotmail.com>
2025-09-18 16:21:48 +0100

    adds typealiases for models

---
+++ Sources/Tapp/Core/NetworkingBridge.swift
@@ -0,0 +1,13 @@
+import Foundation
+import TappNetworking
+
+public typealias Affiliate = TappNetworking.Affiliate
+public typealias AffiliateURLConfiguration = TappNetworking.AffiliateURLConfiguration
+public typealias DeferredLinkDelegate = TappNetworking.DeferredLinkDelegate
+public typealias EventActionMapper = TappNetworking.EventActionMapper
+public typealias EventAction = TappNetworking.EventAction
+public typealias EventConfig = TappNetworking.EventConfig
+public typealias TappError = TappNetworking.TappError
+public typealias TappConfiguration = TappNetworking.TappConfiguration
+
+public typealias Environment = TappNetworking.Environment
